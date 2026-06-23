89ef036dc72c44d215e08b74f94d57dc55f9e7ee
Erza <121083259+Vynts@users.noreply.github.com>
2026-06-23 14:33:37 +0700

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-1" # Ubah sesuai region yang kamu inginkan
+  region = "us-west-2" # Ubah sesuai region yang kamu inginkan
 }
 
 # ==========================================
